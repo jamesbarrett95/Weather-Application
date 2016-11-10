a471f357611018c41bd08989768359205e5413e5
BBD\james.barrett <james.barrett@brightblueday.com>
2016-11-10 16:18:08 +0000

    Retrieved time, weather and wind speed

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+$('#search').keyup(function(e){
+  if(e.keyCode == 13) {
+    $('button').click();
+  }
+});
+
 $('button').click(function() {
-  var city = $('#search').val();
+  var $searchField = $('#search');
+  var $submitButton = $('#button');
+
+  $searchField.prop("disabled", true);
+  // Lodaing functionality not working
+  $submitButton.attr("disabled", true).val("Loading...");
 
   var url = "http://api.openweathermap.org/data/2.5/forecast/weather/";
+  var city = $searchField.val();
 
   var data = {
     q : city,
@@ -9,11 +21,26 @@ $('button').click(function() {
   }
 
   function callback(data) {
+    var div = document.getElementById("result");
     console.log(data);
     $("#result").empty().addClass("border");
-    $("#result").append("The weather for: " + data.city.name);
+    $("#result").append("The weather for: " + data.city.name + "<br>");
+
+    for (var i = 0; i < 5; i++) {
+      var time = data.list[i].dt_txt.slice(10) + " //";
+      var weather = data.list[i].weather[0].description + " //";
+      var windSpeed = "Wind Speed: " + data.list[i].wind.speed;
+      $("#result").append("<p>" + time + ' ' + weather + ' ' + windSpeed + "</p>");
+    }
+
+    $searchField.prop("disabled", false);
+    $submitButton.attr("disabled", false).val("Search");
+
+    var div = document.getElementById("result");
+
+
+
   }
 
   $.getJSON(url, data, callback);
-
 });
