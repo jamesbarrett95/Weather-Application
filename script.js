950cf419710c536cd3e54b85e99134b22b07838b
BBD\james.barrett <james.barrett@brightblueday.com>
2017-08-17 15:51:11 +0100

    Code refactor 1 - Data retrieved using Vanilla JS (utilising async await). Need to display on the screen.

--- script.js
+++ script.js
@@ -3,138 +3,115 @@ window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogn
 const microphoneButton = document.querySelector(".fa-microphone");
 const recognition = new SpeechRecognition();
 
-// Gather buttons and search area
-var $searchField = $('#searcharea');
-var $submitButton = $('#searchbutton');
-var $celsiusButton = $('<button>Celsius</button>');
-var $fahrenheitButton = $('<button>Fahrenheit</button>');
+const searchButton = document.getElementById('searchbutton');
+const searchArea = document.getElementById('searcharea');
+const resultsArea = document.getElementById('result');
+const buttonSection = document.querySelector('.button-section');
+const celsiusButton = document.getElementById('celsiusbutton');
+const fahrenheitButton = document.getElementById('fahrenheitbutton');
+const searchValue = document.querySelector('.searchresult');
 
-recognition.addEventListener('result', e => {
-  const transcript = Array.from(e.results)
-    .map(result => result[0])
-    .map(result => result.transcript)
-    .join('');
-  $searchField.val(transcript);
-  $('button').click();
-});
+const resultAreaChildren = Array.from(document.querySelector('.resultarea').children);
 
-function toggleMicrophone() {
-  recognition.start();
+// This variable is global so we can attach it to our Celsius and Fahrenheit event listeners
+const temperatures = [];
+
+
+function toCelsius() {
+  const temps = Array.from(resultsArea.querySelectorAll('.temp'));
+  temps.map((temp, i) => {
+    temp.innerHTML = temperatures[i];
+  });
 }
 
-// If enter button is clicked, trigger a button click
-$('#searcharea').keyup(function(e){
-  if(e.keyCode == 13) {
-    $('button').click();
-  }
-});
+function toFahrenheit(tempArr) {  
+  const temps = Array.from(resultsArea.querySelectorAll('.temp'));
+  const converted = tempArr.map(temp => {
+    return Math.round(temp * 9 / 5 + 32);  
+  });
 
-// Display Weather
-$('button').click(function() {
+  temps.map((temp, i) => {
+    temp.innerHTML = converted[i];
+  });
+}
 
-  // Grabs all tempeature units
-  var spans = document.getElementsByClassName("temp");
-  // Array to hold current temperature values
-  var temperatureArray = [];
+async function getWeather() {
+  searchButton.disabled = true;
+  searchButton.textContent = 'Loading...';
+  celsiusButton.disabled = true;
+  // URL params
+  const appid = "e384678fabc57f4af277fff449ee339c";
+  const units = "metric";
+  const city = searchArea.value;
 
-  // Disable button whilst data is being fetched
-  $searchField.prop("disabled", true);
-  $submitButton.attr("disabled", true).html("Loading...");
-
-  // AJAX URL and data params
-  var url = "http://api.openweathermap.org/data/2.5/forecast/";
-  var city = $searchField.val();
-  var data = {
-    q : city,
-    appid : "e384678fabc57f4af277fff449ee339c",
-    units : "metric"
-  }
-
-  function toCelsius(arr, spans) {
-    for(var i = 0; i < arr.length; i++) {
-      arr[i] = Math.round((arr[i] - 32) * (5 / 9));
-    }
-    for(var i = 0; i < spans.length; i++) {
-      spans[i].innerHTML = arr[i] + "&#8451";
-    }
-    $fahrenheitButton.attr("disabled", false);
-    $celsiusButton.attr("disabled", true);
-  }
-
-  function toFahrenheit(arr, spans) {
-    for(var i = 0; i < arr.length; i++) {
-      arr[i] = Math.round((arr[i] * (9 / 5)) + 32);
-    }
-    for(var i = 0; i < spans.length; i++) {
-      spans[i].innerHTML = arr[i] + "&#8457";
-    }
-    $celsiusButton.attr("disabled", false);
-    $fahrenheitButton.attr("disabled", true);
-  }
-
-  function averageTemperature(temperatureArray) {
-    var sum = 0;
-    var average;
-    for (var i = 0, j = temperatureArray.length; i < j; i++) {
-      sum += temperatureArray[i];
-    }
-     average = sum / temperatureArray.length;
-     if(average > 20 && average < 25) {
-        $('body').addClass("hot");
-     } else if (average >= 25) {
-        $('body').addClass("very-hot");
-     } else if(average > 7 && average <= 20) {
-        $('body').addClass("cold");
-     } else {
-        $('body').addClass("freezing");
-     }
-  }
-
-  // AJAX Callback
-  function callback(data) {
-      // Add border around result area and display user's city
-      $("#result").empty().addClass("border");
-      $("#result").append("The weather for: " + data.city.name + "<br>");
-
-      // Grab first 5 weather entries and display them on the screen
-      for (var i = 0; i < 5; i++) {
-        var time = data.list[i].dt_txt.slice(10);
-        var temp = Math.round(data.list[i].main.temp);
-        var weather = data.list[i].weather[0].description;
-        var windSpeed = "Wind Speed: " + data.list[i].wind.speed;
-        temperatureArray.push(temp);
-        $("#result").append("<p>" + time + ' // ' + '<span class="temp">' + temp + '&#8451' + '</span>' + ' // ' + weather + ' // ' + windSpeed + "</p>");
-      }
-
-      averageTemperature(temperatureArray);
-
-      $("#result").append($celsiusButton);
-      $("#result").append($fahrenheitButton);
-
-      // Re-enable button after API call is finished
-      $searchField.prop("disabled", false);
-      $submitButton.attr("disabled", false).html("Search");
-
-
-    $celsiusButton.attr("disabled", true);
-
-    $($fahrenheitButton).click(function() {
-      toFahrenheit(temperatureArray, spans);
+  const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast/?q=${city}&appid=${appid}&units=${units}`)
+  const data = await response.json();
+  // console.log(data);
+  searchButton.disabled = false;
+  searchButton.textContent = 'Search';  
+  if(data.cod === "200") {
+    searchValue.textContent = `Results for ${data.city.name}`  
+    buttonSection.style.display = 'block';
+    // Get only the first 5 arrays
+    const newList = data.list.slice(0, 5);
     });
 
-    $($celsiusButton).click(function() {
-      toCelsius(temperatureArray, spans);
-    });
-
+    // resultsArea.innerHTML = newList.map((result, i) => {
+    //   const {dt_txt: time, main: temp, weather, wind} = result;
+    //   temperatures.push(temp.temp);
+    //   return `
+    //     <p>${time} // <span class="temp">${temp.temp}</span> // ${weather[0].description} // ${wind.speed}</p>
+    //   `   
+    // }).join('');
   }
-// get JSON from API
-$.getJSON(url, data, callback)
-  .fail(function() {
-    $("#result").empty().addClass("border");
-    $("#result").append("This area doesn't exist, please try again!");
-    $searchField.prop("disabled", false);
-    $submitButton.attr("disabled", false).html("Search");
-  })
+}
+
+searchButton.addEventListener('click', getWeather);
+searchArea.addEventListener('keyup', (e) => {
+  if(e.keyCode === 13) searchButton.click();
 });
 
-microphoneButton.addEventListener('click', toggleMicrophone);
+//Proper functions used to maintain lexical 'this'
+celsiusButton.addEventListener('click', function() {
+  this.disabled = true;
+  fahrenheitButton.disabled = false;  
+  toCelsius(temperatures);
+});
+fahrenheitButton.addEventListener('click', function() {
+  this.disabled = true;
+  celsiusButton.disabled = false;    
+  toFahrenheit(temperatures);
+});
+
+
+// function toggleMicrophone() {
+//   recognition.start();ton>Celsius</button>');
+// var $fahrenheitButton = $('<button>Fahrenheit</button>');
+
+// recognition.addEventListener('result', e => {
+//   const transcript = Array.from(e.results)
+//     .map(result => result[0])
+//     .map(result => result.transcript)
+//     .join('');
+//   $searchField.val(transcript)
+// }
+
+//   function averageTemperature(temperatureArray) {
+//     var sum = 0;
+//     var average;
+//     for (var i = 0, j = temperatureArray.length; i < j; i++) {
+//       sum += temperatureArray[i];
+//     }
+//      average = sum / temperatureArray.length;
+//      if(average > 20 && average < 25) {
+//         $('body').addClass("hot");
+//      } else if (average >= 25) {
+//         $('body').addClass("very-hot");
+//      } else if(average > 7 && average <= 20) {
+//         $('body').addClass("cold");
+//      } else {
+//         $('body').addClass("freezing");
+//      }
+//   }
+
+// microphoneButton.addEventListener('click', toggleMicrophone);
