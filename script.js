2fc5cec3783c134ee4576c007edd244c28d8d5bd
jamesbarrett95 <james-barrett@hotmail.com>
2017-08-29 17:47:25 +0100

    Code refactor 2 - Script cleaned up and some style changes

--- script.js
+++ script.js
@@ -1,117 +1,126 @@
 // Speech Recognition Variables
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-const microphoneButton = document.querySelector(".fa-microphone");
-const recognition = new SpeechRecognition();
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+const microphoneButton = document.querySelector('.fa-microphone')
+const recognition = new SpeechRecognition()
 
-const searchButton = document.getElementById('searchbutton');
-const searchArea = document.getElementById('searcharea');
-const resultsArea = document.getElementById('result');
-const buttonSection = document.querySelector('.button-section');
-const celsiusButton = document.getElementById('celsiusbutton');
-const fahrenheitButton = document.getElementById('fahrenheitbutton');
-const searchValue = document.querySelector('.searchresult');
+const searchButton = document.getElementById('searchbutton')
+const searchArea = document.getElementById('searcharea')
+const resultsArea = document.getElementById('result')
+const buttonSection = document.querySelector('.button-section')
+const celsiusButton = document.getElementById('celsiusbutton')
+const fahrenheitButton = document.getElementById('fahrenheitbutton')
+const searchValue = document.querySelector('.searchresult')
 
-const resultAreaChildren = Array.from(document.querySelector('.resultarea').children);
-
+const timesContainer = document.querySelector('.times > div:first-of-type')
+const temperatureContainer = document.querySelector('.temperature > div:first-of-type')
+const descriptionContainer = document.querySelector('.description > div:first-of-type')
+const windContainer = document.querySelector('.wind > div:first-of-type')
 // This variable is global so we can attach it to our Celsius and Fahrenheit event listeners
-const temperatures = [];
+let temps = []
 
-
-function toCelsius() {
-  const temps = Array.from(resultsArea.querySelectorAll('.temp'));
-  temps.map((temp, i) => {
-    temp.innerHTML = temperatures[i];
-  });
+function toCelsius () {
+  const tempSpans = Array.from(resultsArea.querySelectorAll('.temp'))
+  tempSpans.map((temp, i) => {
+    temp.innerHTML = temps[i]
+  })
 }
 
-function toFahrenheit(tempArr) {  
-  const temps = Array.from(resultsArea.querySelectorAll('.temp'));
+function toFahrenheit (tempArr) {
+  const tempSpans = Array.from(resultsArea.querySelectorAll('.temp'))
   const converted = tempArr.map(temp => {
-    return Math.round(temp * 9 / 5 + 32);  
-  });
+    return Math.round(temp * 9 / 5 + 32)
+  })
 
-  temps.map((temp, i) => {
-    temp.innerHTML = converted[i];
-  });
+  tempSpans.map((temp, i) => {
+    temp.innerHTML = converted[i]
+  })
 }
 
-async function getWeather() {
-  searchButton.disabled = true;
-  searchButton.textContent = 'Loading...';
-  celsiusButton.disabled = true;
+async function getWeather () {
+  searchButton.disabled = true
+  searchButton.textContent = 'Loading...'
+  celsiusButton.disabled = true
   // URL params
-  const appid = "e384678fabc57f4af277fff449ee339c";
-  const units = "metric";
-  const city = searchArea.value;
-
+  const appid = 'e384678fabc57f4af277fff449ee339c'
+  const units = 'metric'
+  const city = searchArea.value
   const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast/?q=${city}&appid=${appid}&units=${units}`)
-  const data = await response.json();
-  // console.log(data);
-  searchButton.disabled = false;
-  searchButton.textContent = 'Search';  
-  if(data.cod === "200") {
-    searchValue.textContent = `Results for ${data.city.name}`  
-    buttonSection.style.display = 'block';
-    // Get only the first 5 arrays
-    const newList = data.list.slice(0, 5);
-    });
+  const data = await response.json()
+  searchButton.disabled = false
+  searchButton.textContent = 'Search'
+  if (data.cod === '200') {
+    searchValue.innerHTML = `Search result for <span class="highlight">${data.city.name}</span>`
+    resultsArea.style.display = 'flex'
+    buttonSection.style.display = 'block'    
 
-    // resultsArea.innerHTML = newList.map((result, i) => {
-    //   const {dt_txt: time, main: temp, weather, wind} = result;
-    //   temperatures.push(temp.temp);
-    //   return `
-    //     <p>${time} // <span class="temp">${temp.temp}</span> // ${weather[0].description} // ${wind.speed}</p>
-    //   `   
-    // }).join('');
+    // Get only the first 5 arrays
+    const newList = data.list.slice(0, 5)
+
+    // Get data we actually need from the API
+    const times = newList.map(result => result.dt_txt.slice(11))
+    const descriptions = newList.map(result => result.weather[0].description)
+    const temperatures = newList.map(result => result.main.temp)
+    temps = temperatures
+    const windSpeeds = newList.map(result => result.wind.speed)
+
+    // Set innerHTML in results area
+    timesContainer.innerHTML = times.map(time => `<p>${time}</p>`).join('')
+    temperatureContainer.innerHTML = temperatures.map(temp => `<p><span class='temp'>${temp}</span></p>`).join('')
+    descriptionContainer.innerHTML = descriptions.map(desc => `<p>${desc}</p>`).join('')
+    windContainer.innerHTML = windSpeeds.map(wind => `<p>${wind}</p>`).join('')
+
+    averageTemperature(temperatures)
+  } else if (data.cod === '404') {
+    searchValue.innerHTML = `There are no results for <span class='error'>${searchArea.value}</span>`
   }
 }
 
-searchButton.addEventListener('click', getWeather);
+searchButton.addEventListener('click', getWeather)
 searchArea.addEventListener('keyup', (e) => {
-  if(e.keyCode === 13) searchButton.click();
-});
+  if (e.keyCode === 13) searchButton.click()
+})
 
-//Proper functions used to maintain lexical 'this'
-celsiusButton.addEventListener('click', function() {
-  this.disabled = true;
-  fahrenheitButton.disabled = false;  
-  toCelsius(temperatures);
-});
-fahrenheitButton.addEventListener('click', function() {
-  this.disabled = true;
-  celsiusButton.disabled = false;    
-  toFahrenheit(temperatures);
-});
+// Proper functions used to maintain lexical 'this'
+celsiusButton.addEventListener('click', function () {
+  this.disabled = true
+  fahrenheitButton.disabled = false
+  toCelsius(temps)
+})
+fahrenheitButton.addEventListener('click', function () {
+  this.disabled = true
+  celsiusButton.disabled = false
+  toFahrenheit(temps)
+})
 
+function toggleMicrophone () {
+  recognition.start()
 
-// function toggleMicrophone() {
-//   recognition.start();ton>Celsius</button>');
-// var $fahrenheitButton = $('<button>Fahrenheit</button>');
+  recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.results)
+      .map(result => result[0])
+      .map(result => result.transcript)
+      .join('')
+    searchArea.value = transcript
+    // Fake a button click
+    searchButton.click()
+  })
+}
 
-// recognition.addEventListener('result', e => {
-//   const transcript = Array.from(e.results)
-//     .map(result => result[0])
-//     .map(result => result.transcript)
-//     .join('');
-//   $searchField.val(transcript)
-// }
+function averageTemperature (temperatureArray) {
+  document.body.className = ''
+  const total = temperatureArray.reduce((sum, value) => {
+    return sum + value
+  }, 0)
+  const average = total / temperatureArray.length
+  if (average > 20 && average < 25) {
+    document.body.classList.add('hot')
+  } else if (average >= 25) {
+    document.body.classList.add('very-hot')
+  } else if (average > 7 && average <= 20) {
+    document.body.classList.add('cold')
+  } else {
+    document.body.classList.add('freezing')
+  }
+}
 
-//   function averageTemperature(temperatureArray) {
-//     var sum = 0;
-//     var average;
-//     for (var i = 0, j = temperatureArray.length; i < j; i++) {
-//       sum += temperatureArray[i];
-//     }
-//      average = sum / temperatureArray.length;
-//      if(average > 20 && average < 25) {
-//         $('body').addClass("hot");
-//      } else if (average >= 25) {
-//         $('body').addClass("very-hot");
-//      } else if(average > 7 && average <= 20) {
-//         $('body').addClass("cold");
-//      } else {
-//         $('body').addClass("freezing");
-//      }
-//   }
-
-// microphoneButton.addEventListener('click', toggleMicrophone);
+microphoneButton.addEventListener('click', toggleMicrophone)
